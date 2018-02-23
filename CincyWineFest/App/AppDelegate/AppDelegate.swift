8ccaa94eaeeaf770d682cc4042405c729ca9f870
eziegler <eric@zigabytes.com>
2018-02-23 11:59:58 -0500

    EZ: First project commit.

---
+++ CincyWineFest/App/AppDelegate/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  CincyWineFest
+//
+//  Created by Eric Ziegler on 2/23/18.
+//  Copyright © 2018 zigabytes. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    
+    return true
+  }
+
+}
+
---
+++ CincyWineFest/App/ViewControllers/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CincyWineFest
+//
+//  Created by Eric Ziegler on 2/23/18.
+//  Copyright © 2018 zigabytes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+}
+
