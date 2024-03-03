461c38b15db9bdc634f87505bbfc0d99defae0fb
Eric Ziegler <eric@zigamajig.com>
2024-03-02 21:07:49 -0500

    EZ: Initial SwiftUI commit.

---
+++ CincyWineFest/CincyWineFest/App/CincyWineFestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CincyWineFestApp.swift
+//  CincyWineFest
+//
+//  Created by Eric Ziegler on 3/2/24.
+//
+
+import SwiftUI
+
+@main
+struct CincyWineFestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
