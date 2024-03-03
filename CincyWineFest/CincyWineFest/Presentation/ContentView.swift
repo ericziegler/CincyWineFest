da9cd0b0603866d8f5ff5d6a64b31447e421e5fa
Eric Ziegler <eric@zigamajig.com>
2024-03-02 23:46:38 -0500

    EZ: Add fonts.

--- CincyWineFest/CincyWineFest/Presentation/ContentView.swift
+++ CincyWineFest/CincyWineFest/Presentation/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let size: CGFloat = 32
+    
     var body: some View {
         VStack {
             Text("Primary Text")
@@ -18,16 +21,48 @@ struct ContentView: View {
                 .fill(.backgroundSecondary)
                 .frame(maxWidth: .infinity)
                 .frame(height: 20)
-            Text("Secondary Text")
-                .font(.system(size: 19))
+            Text("Henke Winery")
+                .font(.appTitle)
+                .foregroundStyle(.textPrimary)
+            Text("2018 Merlot")
+                .font(.appText)
                 .foregroundStyle(.textSecondary)
+//            Text("Secondary Text")
+//                .font(.system(size: 19))
+//                .foregroundStyle(.textSecondary)
+//            Text("Thin")
+//                .font(.appThin(size: size))
+//                .foregroundStyle(.textPrimary)
+//            Text("Extra Light")
+//                .font(.appExtraLight(size: size))
+//                .foregroundStyle(.textPrimary)
+//            Text("Light")
+//                .font(.appLight(size: size))
+//                .foregroundStyle(.textPrimary)
+//            Text("Regular")
+//                .font(.appRegular(size: size))
+//                .foregroundStyle(.textPrimary)
+//            Text("Medium")
+//                .font(.appMedium(size: size))
+//                .foregroundStyle(.textPrimary)
+//            Text("SemiBold")
+//                .font(.appSemiBold(size: size))
+//                .foregroundStyle(.textPrimary)
+//            Text("Bold")
+//                .font(.appBold(size: size))
+//                .foregroundStyle(.textPrimary)
+//            Text("Extra Bold")
+//                .font(.appExtraBold(size: size))
+//                .foregroundStyle(.textPrimary)
+//            Text("Black")
+//                .font(.appBlack(size: size))
+//                .foregroundStyle(.textPrimary)
             Spacer()
             ZStack {
                 Rectangle()
                     .fill(.app)
                 Text("ACTION")
-                    .font(.system(size: 24))
-                    .fontWeight(.bold)
+                    .font(.actionButton)
                     .foregroundStyle(.white)
             }
             .frame(width: 300, height: 55)
---
+++ CincyWineFest/CincyWineFest/Utilities/Extensions/Font+Extensions.swift
@@ -0,0 +1,72 @@
+//
+//  Font+Extensions.swift
+//  CincyWineFest
+//
+//  Created by Eric Ziegler on 3/2/24.
+//
+
+import SwiftUI
+
+extension Font {
+    
+    // MARK: - Styled Guide Fonts
+    
+    static var appTitle: Font {
+        appMedium(size: 21)
+    }
+    
+    static var appText: Font {
+        appRegular(size: 19)
+    }
+    
+    static var appSubtitle: Font {
+        appRegular(size: 17)
+    }
+    
+    static var actionButton: Font {
+        appBold(size: 22)
+    }
+    
+    static var tab: Font {
+        appMedium(size: 17)
+    }
+    
+    // MARK: - Internal Fonts
+    
+    private static func appThin(size: CGFloat) -> Font {
+        Font.custom("Inter-Thin", size: size)
+    }
+    
+    private static func appExtraLight(size: CGFloat) -> Font {
+        Font.custom("Inter-ExtraLight", size: size)
+    }
+    
+    private static func appLight(size: CGFloat) -> Font {
+        Font.custom("Inter-Light", size: size)
+    }
+    
+    private static func appRegular(size: CGFloat) -> Font {
+        Font.custom("Inter-Regular", size: size)
+    }
+    
+    private static func appMedium(size: CGFloat) -> Font {
+        Font.custom("Inter-Medium", size: size)
+    }
+    
+    private static func appSemiBold(size: CGFloat) -> Font {
+        Font.custom("Inter-SemiBold", size: size)
+    }
+    
+    private static func appBold(size: CGFloat) -> Font {
+        Font.custom("Inter-Bold", size: size)
+    }
+    
+    private static func appExtraBold(size: CGFloat) -> Font {
+        Font.custom("Inter-ExtraBold", size: size)
+    }
+    
+    private static func appBlack(size: CGFloat) -> Font {
+        Font.custom("Inter-Black", size: size)
+    }
+    
+}
